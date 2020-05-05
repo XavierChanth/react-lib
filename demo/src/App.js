38a30a17a3d304475ae7cdc025f66a7a4f0777cb
XavierChanth <xchanthavong@gmail.com>
2020-05-05 14:49:11 -0400

    simplify demo app

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { DarkThemeProvider, Button } from 'react-lib'
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <DarkThemeProvider>
+          <Button>Hi</Button>
+        </DarkThemeProvider>
           Learn React
-        </a>
-      </header>
     </div>
   );
 }
