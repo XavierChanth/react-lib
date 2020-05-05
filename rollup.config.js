a3d4745f6d41c6f0782c0f6cd7172481a7f6dc6f
XavierChanth <xchanthavong@gmail.com>
2020-05-05 16:25:05 -0400

    update building config

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import babel from 'rollup-plugin-babel';
 export default {
   input: 'src/index.js',
   output: {
-    file: './index.js',
+    file: 'bundle/index.js',
     format: 'cjs'
   },
   external: [
